fdabf25343452d5b0b04be5bbd14ee014eadac3f
Artem Podloboshnikov <ArtemPS777@yandex.ru>
2021-10-17 19:17:36 +0300

    First stap

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Link from 'next/link'
+
+const Home: NextPage = () => {
+    
+  return (
+    <>
+    Hello World!
+    </>
+  )
+}
+
+export default Home
